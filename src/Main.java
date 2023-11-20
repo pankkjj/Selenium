5ea3c18392e7c91afc07473191d02be9fb7b0b4a
unknown <pankaj.thakur@codequotient.com>
2023-11-20 17:54:26 +0530

    20/11/2023

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import Quizserver.*;
 
 import org.openqa.selenium.interactions.Actions;
 
+import java.io.IOException;
 import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
@@ -12,24 +13,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     static WebDriver driver=new ChromeDriver();
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         
         Login.login(driver);   
 
         Batch.Findbatch(driver, "https://cqtestga.com/batch/list","Mybatch");
         
-        Set<String> tabs = driver.getWindowHandles();
-        System.out.println(tabs);
+        matchtitle(driver, "Batch Learners | CodeQuotient");
         
-        String currenttab = tabs.iterator().next();
-        tabs.remove(currenttab);
+        Batch.Addbulkusers(driver, driver.getCurrentUrl());
+        
+        // System.out.println(tabs);
+        
+        // String currenttab = tabs.iterator().next();
+        // tabs.remove(currenttab);
 
-        String newtab= tabs.iterator().next();
+        // String newtab= tabs.iterator().next();
 
-        driver.switchTo().window(newtab);
+        // driver.switchTo().window(newtab);
 
-        System.out.println(driver.getCurrentUrl());
-        Batch.Addlearners(driver, driver.getCurrentUrl());
+        // System.out.println(driver.getCurrentUrl());
+        // Batch.Addlearners(driver, driver.getCurrentUrl());
+        Batch.Removelearner(driver, driver.getCurrentUrl(), "learner23@gmail.com");
     }
-    
+
+
+
+    static void matchtitle(WebDriver driver, String title)
+    {
+
+        Set<String> tabs = driver.getWindowHandles();
+
+        for(String tab : tabs)
+        {
+            driver.switchTo().window(tab);
+            
+            if(driver.getTitle().equals(title)){
+                 System.out.println(driver.getCurrentUrl());
+                 break;
+            }
+
+        }
+    }
+
+
 }
--- src/Mainserver/Batch.java
+++ src/Mainserver/Batch.java
@@ -1,5 +1,6 @@
 package Mainserver;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,7 +15,7 @@ import Basicfunctionalities.perform;
 
 public class Batch {
     
-   public static void createbatch(WebDriver driver,String url, String batchname)
+   public static void createbatch(WebDriver driver, String url, String batchname)
    {
        driver.get(url);
        perform.clickButton(driver, "//img[@class=\"cursor-pointer\"]", Duration.ofSeconds(10));
@@ -29,10 +30,7 @@ public class Batch {
        System.out.println("Batch created");
 
     }
-    
-
-
-    
+        
     public static void Findbatch(WebDriver driver, String url, String batchname) throws InterruptedException
     {
         driver.get(url);
@@ -90,34 +88,34 @@ public class Batch {
         perform.clickButton(driver, "//a[@class=\"btn btn-theme mr-3 create-btn tooltip-down\"]", Duration.ofSeconds(10));
          try
             {
-                // Thread.sleep(2000);
-                // List <WebElement> rowslist = driver.findElements(By.xpath("//table[@id=\"mentor-data-table\"]//tr[ @class='even' or @class='odd']"));
+                Thread.sleep(2000);
+                List <WebElement> rowslist = driver.findElements(By.xpath("//table[@id=\"mentor-data-table\"]//tr[ @class='even' or @class='odd']"));
                 
-                // int total=rowslist.size();
-                // if(rowslist.size()<=0)
-                // {
-                //   System.out.println("No questions in my organization to add");
-                //   return;
-                // }
-                // else
-                // {
-                //     System.out.println("total questions in page "+total);
-                //     int count = 1;
-                //     while(count<=3)
-                //     {
-                //         try
-                //         {
-                //             perform.clickButton(driver, "//table[@id=\"mentor-data-table\"]//tr[" + count + "]//td//input[@type='checkbox']", Duration.ofSeconds(10));
+                int total=rowslist.size();
+                if(rowslist.size()<=0)
+                {
+                  System.out.println("No questions in my organization to add");
+                  return;
+                }
+                else
+                {
+                    System.out.println("total questions in page "+total);
+                    int count = 1;
+                    while(count<=3)
+                    {
+                        try
+                        {
+                            perform.clickButton(driver, "//table[@id=\"mentor-data-table\"]//tr[" + count + "]//td//input[@type='checkbox']", Duration.ofSeconds(10));
                           
-                //             count++;
-                //         }
-                //         catch(Exception e)
-                //         {
-                //             System.out.println("again here");   
-                //             System.out.println("Stale Exception");                
-                //         }
-                //     }
-                // } 
+                            count++;
+                        }
+                        catch(Exception e)
+                        {
+                            System.out.println("again here");   
+                            System.out.println("Stale Exception");                
+                        }
+                    }
+                } 
              perform.clickButton(driver,  " //button[@type='button'][normalize-space()='Save']", Duration.ofSeconds(10));
             }
             catch(Exception e){
@@ -126,4 +124,23 @@ public class Batch {
 
 
     }
+     
+    public static void Addbulkusers(WebDriver driver, String url) throws IOException, InterruptedException{
+        driver.get(url);
+        perform.clickButton(driver, "//button[@data-customtooltip=\"Bulk add users\"]",  Duration.ofSeconds(10));
+        perform.clickButton(driver, "//label[@for=\"upldFile\"]", Duration.ofSeconds(10));
+
+        String path="C:\\Users\\Pankaj Thakur\\Documents\\script.au3";
+        ProcessBuilder processbuilder = new ProcessBuilder("C:\\Program Files (x86)\\AutoIt3\\AutoIt3.exe", path);
+        Process process = processbuilder.start();
+        process.waitFor();
+        perform.clickButton(driver, "//button[@type=\"submit\"][normalize-space()=\"Upload\"]", Duration.ofSeconds(10));
+        }
+
+    public static void Removelearner(WebDriver driver, String url, String email){
+       
+        driver.get(url);
+        perform.sendKeys(driver, "//input[@placeholder=\"Search in batch\"]", Duration.ofSeconds(10), email);
+
+    }    
 }
